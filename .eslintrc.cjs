934b634e8d22b1ce4e4815ec1f11a762413922a3
tuhin_4ever <tuhin4ever19@gmail.com>
2023-05-17 20:49:17 +0600

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // Add your custom rules here
+  },
+};
